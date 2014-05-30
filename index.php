a40d12a5417d454220a6eb0ce7d596acb64d7629
ljearwood <ljearwood@gmail.com>
2014-05-30 08:55:25 -0500

    Friday the 30th morning prototyping

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     while( $my_query->have_posts() ) : $my_query->the_post();
     ?>
         <h2 class="show-for-medium-up text-center" itemprop="headline"><?php echo $titleString; ?></h2>
-        <h2  class="show-for-small text-center" itemprop="headline">Compusign</h2>
+        <h2 class="show-for-small text-center">Compusign</h2>
         <?php //var_dump( $postID['main_description'] ); ?>
         <hr />
         <?php  //JasperDisplayOrbit(); ?>
@@ -26,8 +26,46 @@
             <h1 class="small-12 large-12 columns text-center entry-title" itemprop="name"><span id="numero1" itemprop="headline"><a rel-bookmark href="<?php the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></span></h1>
             <?php Jasper_entry_meta(); ?>
         </div>
+    <div class="small-3 large-3 columns">
+        <div class="panel">
+            <h2>Welcome to Our site</h2>
+            <hr/>
+            <h3>Helpful links</h3>
+            <ul>
+                <li><a href="#">Lorem ipsum dolor.</a></li>
+                <li><a href="#">Ducimus, itaque, praesentium.</a></li>
+                <li><a href="#">Amet, deserunt, esse.</a></li>
+                <li><a href="#">Dolorum, harum, voluptatibus.</a></li>
+                <li><a href="#">Aperiam, ducimus, eius.</a></li>
+            </ul>
+        </div>
+        <div class="panel">
+            <h3>Get a Quote!</h3>
 
-    <div id="firstStop" class="small-9 large-8 columns panel">
+            <form action="#" method="post">
+                <div class="row">
+                    <div class="large-12 columns">
+                        <div class="row collapse">
+                            <div class="small-8 columns">
+                                <input type="text" placeholder="Email">
+                            </div>
+                            <div class="small-4 columns">
+                                <a href="#" class="button postfix">Quote</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="panel">
+            <h3>Lorem ipsum dolor sit amet.</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta eius modi saepe!</p>
+            <h4><a href="#">Some Useful link</a>
+            </h4>
+            <p>Debitis error hic iure nemo perspiciatis quaerat, quam quas quo? Laudantium, tempore!</p>
+        </div>
+    </div>
+    <div id="firstStop" class="small-12 large-6 columns panel">
         <?php the_content(); ?>
     <?php
             endwhile;
@@ -35,9 +73,10 @@
         wp_reset_postdata();
     ?>
     </div>
-    </article>
-    <div class="small-3 large-4 columns show-for-medium-up">
+    <div class="small-12 large-3 columns right">
         <?php get_sidebar(); ?>
     </div>
+</article>
+
 </div>
 <?php get_footer(); ?>
